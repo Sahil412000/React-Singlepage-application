7e9dce70cb801e734b0b41e0c900b3f5271c14bb
Sahil Punjabi <sahilpanjab123@gmail.com>
2020-07-21 22:59:01 +0530

    React SinglePage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menucomponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return(
+      <BrowserRouter>
+      <div className="App">
+         <Main/>
+       </div> 
+       </BrowserRouter>
+    );
+  }
 }
 
 export default App;
